1af0d3e607d6199dc893af3d7530484cd4e4ab11
mohammad hammade <medo.spider0000@gmail.com>
2020-08-10 15:18:20 +0300

    Make job_list and job_details Views using django template language

--- job/views.py
+++ job/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import job
 
 # Create your views here.
 def job_list(request):
-    pass
+
+    #Model QuerySet Instead of db query (select * from job)
+    job_list = job.objects.all()
+    
+    # jobs is the variable that used in template
+    #context must be dict
+    context = {'jobs':job_list}
+    return render(request,'job/job_list.html',context)
 
 def job_details(request, id):
-    pass
+    job_details = job.objects.get(id=id)
+    context = {'job': job_details}
+    return render(request,'job/job_details.html',context)
