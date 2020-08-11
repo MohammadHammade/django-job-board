02822e0d4baebe44724ed472018fd021286d1517
mohammad hammade <medo.spider0000@gmail.com>
2020-08-11 03:54:13 +0300

    add django pagination

--- job/views.py
+++ job/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import job
+from django.core.paginator import Paginator
+from django.shortcuts import render
 
 # Create your views here.
 def job_list(request):
@@ -7,9 +9,15 @@ def job_list(request):
     #Model QuerySet Instead of db query (select * from job)
     job_list = job.objects.all()
     
+    #for pagination
+    n = 1
+    paginator = Paginator(job_list,n) #shoq n page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     # jobs is the variable that used in template
     #context must be dict
-    context = {'jobs':job_list}
+    context = {'jobs':job_list,'page':page_obj }
     return render(request,'job/job_list.html',context)
 
 def job_details(request, id):
